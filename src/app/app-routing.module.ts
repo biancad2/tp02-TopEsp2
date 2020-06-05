3dade1b9ce8e36e9915098dd33a9260771811632
Gabrielli Correa <gabriellicorrea7@gmail.com>
2020-06-04 21:55:45 -0300

    [*GABRIELLI* componente bitcoin BR]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { BitcoinComponent } from './bitcoin/bitcoin.component';
+import { BitcoinBrComponent } from './bitcoinbr/bitcoinbr.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'bitcoin', component: BitcoinComponent },
+  { path: 'bitcoinbr', component: BitcoinBrComponent },
+
 ];
 
 @NgModule({
